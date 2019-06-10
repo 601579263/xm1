f47b7898e2b0d085a105ef0b6c00324abe45bae5
Administrator <admin@example.com>
2019-06-10 22:39:05 +0800

    阿达

---
+++ dmg-jenkins/src/main/java/com/dmg/Appjenkis.java
@@ -0,0 +1,12 @@
+package com.dmg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Appjenkis {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Appjenkis.class, args);
+	}
+}
